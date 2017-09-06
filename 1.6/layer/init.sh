f0beba2e9740fedb1f513d621b2756f0a2acd3f8
Jake Wise <jakewise93@gmail.com>
2017-09-06 22:54:10 +0100

    Updated confighub to version 1.6.2.

--- 1.6/layer/init.sh
+++ 1.6/layer/init.sh
@@ -4,8 +4,8 @@ if [ ! -z "${DB_USERNAME}" ] && [ ! -z "${DB_PASSWORD}" ]; then
     export DB_DRIVER=${DB_DRIVER:-com.mysql.jdbc.Driver}
     export DB_URL=${DB_URL:-jdbc:mysql://db:3306}
 
-    echo "Writing database credentials to /confighub-v1.6/server/conf/tomee.xml"
-    envsubst < /var/tpl/tomee.xml > /confighub-v1.6/server/conf/tomee.xml
+    echo "Writing database credentials to /confighub/server/conf/tomee.xml"
+    envsubst < /var/tpl/tomee.xml > /confighub/server/conf/tomee.xml
 fi
 
 supervisord --nodaemon --configuration /etc/supervisord.conf
