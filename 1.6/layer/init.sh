685b4adf9c90b33c8099b95712f032345039af04
Edin Arslanagic <edoarsla@gmail.com>
2017-09-13 11:24:25 -0500

    testing...

--- 1.6/layer/init.sh
+++ 1.6/layer/init.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
 
-#if [ ! -z "${DB_USERNAME}" ] && [ ! -z "${DB_PASSWORD}" ]; then
 export DB_DRIVER=${DB_DRIVER:-com.mysql.jdbc.Driver}
 export DB_URL=${DB_URL:-jdbc:mysql://192.168.1.115:3306}
+export DB_MAIN_NAME=${DB_MAIN_NAME:-ConfigHubMain}
 
 echo "Writing database credentials to /confighub/server/conf/tomee.xml"
 envsubst < /var/tpl/tomee.xml > /confighub/server/conf/tomee.xml
-#fi
+
 
 export ALLOCATED_MEMORY=${ALLOCATED_MEMORY:-4g}
 export HTTP_PORT=${HTTP_PORT:-80}
