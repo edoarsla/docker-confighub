80a74e42e6f0c821b1a2b0fbf0fcaf9e6e06c903
Edin Arslanagic <edoarsla@gmail.com>
2017-09-14 09:53:08 -0500

    testing...

--- 1.6/layer/init.sh
+++ 1.6/layer/init.sh
@@ -1,12 +1,17 @@
 #!/usr/bin/env bash
 
-export DB_DRIVER=${DB_DRIVER:-com.mysql.jdbc.Driver}
-export DB_URL=${DB_URL:-jdbc:mysql://192.168.1.115:3306}
-export DB_MAIN_NAME=${DB_MAIN_NAME:-ConfigHubMain}
+if [ -z ${DB_MAIN_DRIVER+x} ]; then echo "DB_MAIN_DRIVER not set!" exit 1; fi
+if [ -z ${DB_MAIN_URL+x} ]; then echo "DB_MAIN_URL not set!" exit 1; fi
+if [ -z ${DB_MAIN_USERNAME+x} ]; then echo "DB_MAIN_USERNAME not set!" exit 1; fi
+if [ -z ${DB_MAIN_PASSWORD+x} ]; then echo "DB_MAIN_PASSWORD not set!" exit 1; fi
+
+if [ -z ${DB_API_DRIVER+x} ]; then echo "DB_API_DRIVER not set!" exit 1; fi
+if [ -z ${DB_API_URL+x} ]; then echo "DB_API_URL not set!" exit 1; fi
+if [ -z ${DB_API_USERNAME+x} ]; then echo "DB_API_USERNAME not set!" exit 1; fi
+if [ -z ${DB_API_PASSWORD+x} ]; then echo "DB_API_PASSWORD not set!" exit 1; fi
 
 echo "Writing database credentials to /confighub/server/conf/tomee.xml"
 envsubst < /var/tpl/tomee.xml > /confighub/server/conf/tomee.xml
-
 
 export ALLOCATED_MEMORY=${ALLOCATED_MEMORY:-4g}
 export HTTP_PORT=${HTTP_PORT:-80}
@@ -16,7 +21,7 @@ export KEYSTORE_FILE=${KEYSTORE_FILE:-cert/confighub_default.jks}
 export KEYSTORE_ALIAS=${KEYSTORE_ALIAS:-confighub}
 export KEYSTORE_PASSWORD=${KEYSTORE_PASSWORD:-confighub}
 
-echo "Writing database credentials to /confighub/server/conf/tomee.xml"
+echo "Writing server configuration to confighub/confighub.sh"
 envsubst < /var/tpl/confighub.sh > /confighub/confighub.sh
 
 supervisord --nodaemon --configuration /etc/supervisord.conf
